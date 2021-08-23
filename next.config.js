d40732d7f26494750a5c7c4a0c61492077f56751
ashwindevx <ashwinchauhan.developer@gmail.com>
2021-08-23 18:14:53 +0530

    files

---
+++ next.config.js
@@ -0,0 +1,31 @@
+const { BLOG_URL, TESTS_URL } = process.env;
+
+module.exports = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+      },
+      // Blog
+      {
+        source: '/blog',
+        destination: `${BLOG_URL}/blog`,
+      },
+      {
+        source: '/blog/:path*',
+        destination: `${BLOG_URL}/blog/:path*`,
+      },
+      // Tests
+      {
+        source: '/tests',
+        destination: `${TESTS_URL}/tests`,
+      },
+      {
+        source: '/tests/:path*',
+        destination: `${TESTS_URL}/tests/:path*`,
+      },
+    ]
+  },
+}
